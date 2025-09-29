e23d4583467d11c49a3ed3f3a6087b57fe6933f7
unknown <valeriya9169566@gmaie.com>
2025-09-29 21:17:41 +0300

    Oleg do tasks-1-3

---
+++ tasks/task1.py
@@ -0,0 +1,11 @@
+num1 = 5
+num2 = 3
+print(num1)
+print(num2)
+
+
+num3 = num2
+num2 = num1
+num1 = num3
+print(num1)
+print(num2)
---
+++ tasks/task2.py
@@ -0,0 +1,10 @@
+num1 = 135
+num2 = 135//60
+num3 = 135%
+print(num2, num3)
+
+
+minutes = int(input(" Ведите количество  минут:"))
+hours = minutes // 60
+last_minutes = minutes % 60
+print(f"{minutes} минут это {hours} часов и {last_minutes} минут")
