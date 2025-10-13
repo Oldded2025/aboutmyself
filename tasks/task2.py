b5cd5a20e8c53ec22118bab5156ffa1676ad4167
kavitosik <km220410@gmail.com>
2025-10-13 19:27:59 +0300

    добавили пробелы

--- tasks/task2.py
+++ tasks/task2.py
@@ -8,3 +8,12 @@ minutes = int(input(" Ведите количество  минут:"))
 hours = minutes // 60
 last_minutes = minutes % 60
 print(f"{minutes} минут это {hours} часов и {last_minutes} минут")
+
+
+
+
+
+
+
+
+
