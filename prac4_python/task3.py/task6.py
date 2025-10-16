b967d82a1e6f13bb5110c0d1e54e13b7576a75dc
olddedd2025 <dedovop@gmail.com>
2025-10-16 10:07:16 +0300

    Сделал задачу

---
+++ prac4_python/task3.py/task6.py
@@ -0,0 +1,15 @@
+number = int( input( "Введите число "))
+if number > 0 :
+    sign = "Положительное"
+elif number < 0 :
+    sign = "Отрицательное"
+else :
+        sign = 0
+if number %2 ==0
+     chet_nachet = "чётное"  
+else
+     chet_ nechet = "нечётное" 
+if number == 0
+    print(f"sign")
+ else:
+    print(f" 9sign, (chet_nechet)")   
